e6882d37b430dee95a7bb8a8d4dee112f402521e
tangyabo <13316022418@163.com>
2024-05-30 15:46:17 +0800

    Initial commit

---
+++ src/test/java/com/yaber/elk/esdemo/EsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaber.elk.esdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
